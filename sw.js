af4f42b8b1543b67439fad0ccc89bd3bac0b87cb
Clinician <apk.saadalhamady@gmail.com>
2026-09-02 12:48:31 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'albumintrak-v2';
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll([
+        './index.html',
+        './manifest.json',
+        './icon.svg'
+    ])));
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
